e5f3b8de2e36ce2e57e7c11cba17ffe53195b923
root <root@SOFT005855.softplan.com.br>
2023-01-22 18:28:41 -0300

    Ajuste main

---
+++ resources/views/app/home.blade.php
@@ -0,0 +1,352 @@
+@extends('app.layouts.main')
+
+@section('content')
+    <section id="hero" class="d-flex justify-content-center">
+        <div class="box-hero" style="z-index: 100;">
+
+            <div class="box-table">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Data</th>
+                            <th scope="col">Horário</th>
+                            <th scope="col">Placa</th>
+                            <th scope="col">Contato</th>
+                            <th scope="col">Editar</th>
+                            <th scope="col">Deletar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>
+                                <button class="btn btn-sm btn-success">Editar</button>
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Deletar</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('site');
+});
+
+Route::get('/home', function () {
+    return view('app.home');
 });
